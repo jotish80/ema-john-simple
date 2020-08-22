c3d33e7fa71f5b69abe4ecd549c7ef22e2f6ee93
jotish80 <sjotishchandra@gmail.com>
2020-08-23 01:22:53 +0200

    shoping card added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
-const Product = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+const Product = (props) => {
+    console.log(props.product.name);
+    const { img, name, seller, stock } = props.product;
     return (
-        <div>
-            <h3>This is product</h3>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="product-name">
+                <h4>{name}</h4>
+                <br />
+                <p><small>by:{seller}</small></p>
+                <p>$price</p>
+                <br />
+                <p><small>Only {stock} left in stock - order soon</small></p>
+                <button className="main-button"> <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+            </div>
         </div>
     );
 };
--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './shop.css';
-
+import Product from '../Product/Product';
 const Shop = () => {
  const first10 = fakeData.slice(0,10);
  const [products, setProducts] = useState(first10);
@@ -10,7 +10,7 @@ const Shop = () => {
         <div className ="shop-container">
             <div className="product-container">
             {
-                products.map(product => <Product>{product.name}</Product>)
+                products.map(pd => <Product product = {pd}></Product>)
             }
             </div>
             <div className="cart-container">
